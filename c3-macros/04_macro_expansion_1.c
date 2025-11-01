f784054db186fc64e66a527c856b6b0b8fe55fed
Pavel Tisnovsky <ptisnovs@redhat.com>
2025-11-01 09:10:45 +0100

    Macro expansion in C - semi-correct variant

---
+++ c3-macros/04_macro_expansion_1.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+#define add_mul(x, y) (x)*(y)+1
+
+int sqr(int x)
+{
+    return x*x;
+}
+
+void main(void)
+{
+    int x = 1;
+    int y = 2;
+
+    int z = add_mul(x+y, x+y);
+    int w = add_mul(x+y, x+y)*2;
+
+    printf("%d\n", z);
+    printf("%d\n", w);
+}
