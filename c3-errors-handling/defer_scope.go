3e56a2407995f789db65e5cce0bea24144ad2382
Pavel Tisnovsky <ptisnovs@redhat.com>
2025-12-10 08:39:12 +0100

    Link to article

--- c3-errors-handling/defer_scope.go
+++ c3-errors-handling/defer_scope.go
@@ -1,3 +1,13 @@
+/*
+ * Seriál Programovací jazyk C3
+ * https://www.root.cz/serialy/programovaci-jazyk-c3/
+ * --------------------------------------------------
+ *
+ * Demonstrační příklad použitý v článku:
+ * Programovací jazyk C3: reakce na chyby, odložení vykonání příkazů
+ * https://www.root.cz/clanky/programovaci-jazyk-c3-reakce-na-chyby-odlozeni-vykonani-prikazu/
+ */
+
 package main
 
 import "fmt"
